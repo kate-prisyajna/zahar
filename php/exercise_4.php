5846aa5fca993093a53bab1351bb0d0233efd4ce
prisyajna@prisyajna <prysiazhna@rozetka.com.ua>
2021-07-07 21:16:27 +0300

    ex 4

--- php/exercise_4.php
+++ php/exercise_4.php
@@ -9,6 +9,26 @@ echo <<<'EOD'
 Ответ: 
 
 
+$arr=array(0=>1.2, 1=>3, 2=>-3.2, 4=>1.34, 5=>-0.3, 6=>1.2, 7=>5.3, 8=>0.67);
+
+echo ">Массив без сортироваки:";
+foreach ($arr as $key=>$value) {
+    print "Ключ=$key | Значение=$value";
+}
+array_pop($arr);
+
+echo "Предпоследний елемент: ";
+print_r(end($arr));
+
+echo "Массив с сортировкой:";
+natsort($arr);
+foreach ($arr as $key=>$value) {
+    print "Ключ=$key | Значение=$value";
+}
+array_pop($arr);
+
+echo "Предпоследний елемент: ";
+print_r(max($arr));
 
 ----------------------------------------------------------------------------------------------------------------------
 
@@ -17,16 +37,26 @@ echo <<<'EOD'
 
 EOD;
 
-$arr=array(0=>1.2, 1=>3, 2=>-3.2, 4=>1.34, 5=>-0.3, 6=>1.2, 7=>1.93, 8=>0.67);
+$arr=array(0=>1.2, 1=>3, 2=>-3.2, 4=>1.34, 5=>-0.3, 6=>1.2, 7=>5.3, 8=>0.67);
+
+echo "<br>Массив без сортироваки:<br>";
 foreach ($arr as $key=>$value) {
-    if ($value == max($arr)) {
-        print   "Максимальное число по величине:    Ключ=$key | Значение=$value\n";
-        $a=$key - 1;
-        print_r("Предыдущий элемент $arr[$a]");
-    }
-
+    print "Ключ=$key | Значение=$value<br>";
 }
+array_pop($arr);
 
+echo "<br>Предпоследний елемент: ";
+print_r(end($arr));
+
+echo "<br><br>Массив с сортировкой:<br>";
+natsort($arr);
+foreach ($arr as $key=>$value) {
+    print "Ключ=$key | Значение=$value<br>";
+}
+array_pop($arr);
+
+echo "<br>Предпоследний елемент: ";
+print_r(max($arr));
 
 
 
